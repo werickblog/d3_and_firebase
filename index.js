c5f3fa943cc6d5958e424f4cc4e28d18f76addfd
tesh254 <ewachira254@gmail.com>
2019-12-06 20:25:41 +0300

    was able to define more methods that you can use from d3

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const data = [
-  { width: 200, height: 100, fill: "purple" },
+  { width: 200, height: 100, fill: "pink" },
   { width: 100, height: 60, fill: "yellow" },
-  { width: 80, height: 30, fill: "pink" },
+  { width: 80, height: 30, fill: "indigo" }
 ];
 
 const svg = d3.select("svg");
 
-const rect = svg
-  .selectAll("rect")
-  .data(data)
-  .attr("width", (d, i, n) => {
-    return d.width;
-  })  
-  .attr("height", d => {
-    return d.height;
-  })
-  .attr("fill", d => {
-    return d.fill;
-  });
+// Join the data to rects
+const rects = svg.selectAll("rect").data(data);
+
+// Append attributes to items already in the dom
+rects
+  .attr("width", (d, i, n) => d.width)
+  .attr("height", d => d.height)
+  .attr("fill", d => d.fill);
+
+// Append attributes to items virtually in the dom
+rects
+  .enter()
+  .append("rect")
+  .attr("width", (d, i, n) => d.width)
+  .attr("height", d => d.height)
+  .attr("fill", d => d.fill);
