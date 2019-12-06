cc8773cfef67bc4defdd5080ae2f517a78569711
tesh254 <ewachira254@gmail.com>
2019-12-06 20:12:04 +0300

    was able to define basics of creating a vector with d3

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
-/**
- * What d3.select does is wrap the first div with a
- * Selection object  which is returned and
- * is assigned to the assigned variable
- */
+const data = [
+  { width: 200, height: 100, fill: "purple" },
+  { width: 100, height: 60, fill: "yellow" },
+  { width: 80, height: 30, fill: "pink" },
+];
 
-const canvas = d3.select(".canvas");
+const svg = d3.select("svg");
 
-/**
- * Append a new element in the wrapped element
- */
-const svg = canvas.append("svg");
-
-// Add attributes to the svg element
-svg.attr("height", 600);
-svg.attr("width", 600);
-
-// Create a group wrapper
-const group = svg
-  .append("g")
-  // Move the grouped vectors along the x and y axis
-  .attr("transform", "translate(0, 100)");
-
-// Append shapes into the svg tag
-group
-  .append("rect")
-  .attr("height", 200)
-  .attr("width", 100)
-  .attr("fill", "pink")
-  .attr("x", 20)
-  .attr("y", 40);
-group
-  .append("circle")
-  .attr("r", 60)
-  .attr("cx", 70)
-  .attr("cy", 100)
-  .attr("fill", "indigo");
-group
-  .append("line")
-  .attr("x1", 20)
-  .attr("y1", 40)
-  .attr("x2", 120)
-  .attr("y2", 240)
-  .attr("stroke", "blue")
-  .attr("stroke-width", 3);
-group
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 40)
-  .attr("fill", "black")
-  .text("hello world! climate change is real")
-  .style("font-family", "arial");
+const rect = svg
+  .selectAll("rect")
+  .data(data)
+  .attr("width", (d, i, n) => {
+    return d.width;
+  })  
+  .attr("height", d => {
+    return d.height;
+  })
+  .attr("fill", d => {
+    return d.fill;
+  });
